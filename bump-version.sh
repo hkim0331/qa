9de1ff44f661dcf8930e5935fa5c824379fb047a
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2021-06-18 10:49:42 +0900

    add bump-version.sh

---
+++ bump-version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+    echo "usage: $0 <version>"
+    exit
+fi
+
+SED="/bin/sed"
+if [ -x "${HOMEBREW_PREFIX}/bin/gsed" ]; then
+    SED=${HOMEBREW_PREFIX}/bin/gsed
+fi
+
+${SED} -E -i "s/^\(defproject (.+) .+/(defproject \1 \"$1\"/" project.clj
