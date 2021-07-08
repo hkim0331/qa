8c85341454ee82bd2f17aff259722370c8b2ef60
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2021-07-08 15:02:13 +0900

    answer でのみ、<br> が使える。

--- src/qa/view/page.clj
+++ src/qa/view/page.clj
@@ -9,7 +9,7 @@
   [ring.util.anti-forgery :refer [anti-forgery-field]]
   [taoensso.timbre :as timbre :refer [debug]]))
 
-(def version "0.4.1")
+(def version "0.4.2")
 
 (defn unescape-br
   "文字列 s 中のすべての &lt;br を<br でリプレースバック。"
