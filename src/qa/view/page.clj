d9f91691143c2d84e0c0cf29c238ac5e53a812ab
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-10-16 09:54:39 +0900

    release 0.7.0

--- src/qa/view/page.clj
+++ src/qa/view/page.clj
@@ -10,7 +10,7 @@
   [ring.util.anti-forgery :refer [anti-forgery-field]]
   [taoensso.timbre :as timbre :refer [debug]]))
 
-(def version "0.6.9")
+(def version "0.7.0")
 
 (defn unescape-br
   "文字列 s 中のすべての &lt;br を<br でリプレースバック。"
