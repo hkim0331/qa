0dca7e5e14ba9acd099f74f12d6482ecfd5ec21e
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2022-03-25 10:58:39 +0900

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+	echo usage: $0 target/file.jar
+	exit 1
+fi
+BN=`basename $1`
+scp $1 app.melt:qa/ && \
+ssh app.melt "(cd qa && ln -sf ${BN} qa.jar)" && \
+ssh app.melt sudo systemctl restart qa
+
