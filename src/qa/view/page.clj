55bd5709a009a5079b23fd0fca52f1a46b8e3a20
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-04-26 22:32:24 +0900

    1.3.8

--- src/qa/view/page.clj
+++ src/qa/view/page.clj
@@ -11,7 +11,7 @@
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [taoensso.timbre :as timbre]))
 
-(def version "1.3.8-SNAPSHOT")
+(def version "1.3.8")
 
 ;; from r99c.route.home/wrap
 (defn- wrap-aux
