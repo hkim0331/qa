08ea08916dbe3e31483a6f0f039f2bfee7cf1ddf
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-06-25 12:56:55 +0900

    import l22/db/restore.sh

--- db-dumps/restore.sh
+++ db-dumps/restore.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+# 2022-05-07 --last option
+
+DB=l22
+
 if [ -z "$1" ]; then
-  echo "usage: $0 file.sql"
-  exit
+    echo "usage: $0 yyyy-mm-dd.sql"
+    echo "       $0 --last"
+    echo restore postgresql database from ${DB}-yyyy-mm-dd.sql
+    echo mind: this script drops database first.
+    exit 1
+elif [ "--last" = "$1" ]; then
+    DUMP=`ls -t ${DB}* | head -1`
+else
+    DUMP=$1
 fi
 
-psql -h localhost -U postgres -W l22 < $1
+PSQL="psql -h localhost -U postgres"
+${PSQL} -c "drop database ${DB}"
+${PSQL} -c "create database ${DB} owner='postgres'"
+${PSQL} ${DB} < ${DUMP}
+
