3cbb77974ade7a6c955e1ff9d188410d5d766bd2
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-09-12 22:16:19 +0900

    empty textarea after submitting

--- src/qa/middleware.clj
+++ src/qa/middleware.clj
@@ -15,19 +15,18 @@
   (if (authenticated? request)
     (do
       (info "unauthorized-handler: autenticated")
-      (throw-unauthorized)) ;{:status 403 :body "error"}
+      (throw-unauthorized))
     (do
-      ;; ここに入っちゃう。
       (info "unauthorized-handler: unauthenticated")
       [::response/found  "/login"])))
 
 (def auth-backend
   (session-backend {:unauthorized-handler unauthorized-handler}))
 
-(defn probe [handler]
- (fn [req]
-  (info "probe session identity:" (get-in req [:session :identity]))
-  (handler req)))
+;; (defn probe [handler]
+;;  (fn [req]
+;;   (info "probe session identity:" (get-in req [:session :identity]))
+;;   (handler req)))
 
 (defmethod ig/init-key :qa.middleware/auth [_ _]
   (fn [handler]
