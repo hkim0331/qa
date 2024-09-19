e03f032ba3bd4e1f5b06d994e9a1cd7f4a165c01
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-09-20 06:07:07 +0900

    stop keywordize

--- src/qa/middleware.clj
+++ src/qa/middleware.clj
@@ -8,13 +8,15 @@
    [buddy.auth.middleware :refer [wrap-authorization wrap-authentication]]
    [integrant.core :as ig]
    #_[ring.middleware.params :refer [wrap-params]]
-   [taoensso.timbre :refer [info]]))
+   [taoensso.timbre :refer [info debug]]))
 
 (defn unauthorized-handler
   [request _]
+  (debug "request session identity" (get-in [:session :identity] request))
+  (debug "authenticated?" (authenticated? request))
   (if (authenticated? request)
     (do
-      (info "unauthorized-handler: autenticated")
+      (info "unauthorized-handler: authenticated")
       (throw-unauthorized))
     (do
       (info "unauthorized-handler: unauthenticated")
@@ -23,15 +25,15 @@
 (def auth-backend
   (session-backend {:unauthorized-handler unauthorized-handler}))
 
-;; (defn probe [handler]
-;;  (fn [req]
-;;   (info "probe session identity:" (get-in req [:session :identity]))
-;;   (handler req)))
+(defn probe [handler]
+ (fn [req]
+  (debug "probe session identity:" (get-in req [:session :identity]))
+  (handler req)))
 
 (defmethod ig/init-key :qa.middleware/auth [_ _]
   (fn [handler]
     (-> handler
-        ;; probe
         (restrict {:handler authenticated?})
         (wrap-authorization  auth-backend)
-        (wrap-authentication auth-backend))))
+        (wrap-authentication auth-backend)
+        probe)))
